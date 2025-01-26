913270f028dcc04ccc2180ef95ad2bca85541620
qobiljon <qobiljon@gmail.com>
2025-01-26 22:25:56 +0500

    Add initial HTML structure and JavaScript file for JS-7 exercises

---
+++ app.js
@@ -0,0 +1,164 @@
+/* For 5.*/
+
+// let a = 2;
+// let b = 8;
+// let counter = 0;
+
+// for (let i = a; i <= b; i++) {
+//   counter += i;
+// }
+// console.log(`Yig'indi ${counter}`);
+
+/* For 6.*/
+// let a = 1;
+// let b = 3;
+// let counter = 1;
+
+// for (let i = a; i <= b; i++) {
+//   counter *= i;
+// }
+// console.log(`Yig'indi ${counter}`);
+
+/* For 7.*/
+// let a = 1;
+// let b = 9;
+// let counter = 0;
+
+// for (let i = a; i <= b; i++) {
+//   counter += i ** 2;
+// }
+// console.log(`Yig'indi ${counter}`);
+
+/* For 8.*/
+
+// let n = 6;
+// let content = 1;
+// for (let i = 1 / n; i <= 1 / n; i++) {
+//   content += i;
+// }
+// console.log(`Yig'indi ${content}`);
+
+/* For 9.*/
+// let n = 3;
+// let counter = 1;
+// for (let i = 1; i <= n; i++) {
+//   counter *= 1 + i / 10;
+// }
+// console.log(`Yeg'indi ${counter}`);
+
+/* For 10.*/
+
+// let n = 4;
+// let counter = 0;
+// for (let i = 1; i <= n; i++) {
+//   console.log(`${i}`);
+//   for (let k = 1; k <= i; k++) {
+//     let sum = 2 * k - 1;
+//     counter += sum;
+//     console.log(`${sum}  ${counter}`);
+//   }
+//   console.log(`${i}**2 = ${counter}`);
+// }
+
+/* For 11.*/
+// let n = 5;
+// let a = 2;
+// let sum = 1;
+// for (let i = 1; i <= n; i++) {
+//   sum *= a;
+// }
+// console.log(sum);
+
+/* For 12.*/
+
+// let n = 4;
+// let a = 2;
+// let sum = 1;
+// for (let i = 1; i <= n; i++) {
+//   sum = i ** a;
+//   console.log(sum);
+// }
+
+/* For 13.*/
+
+// let n = 4;
+// let a = 2;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += a ** i;
+// }
+// console.log(sum);
+
+/* For 14.*/
+
+// let n = 4;
+// let S = 0;
+// let factr = 1;
+// for (let i = 1; i <= n; i++) {
+//   factr *= i;
+//
+// }
+// console.log(`Yig'indi ${factr}`);
+
+/* For 15.*/
+
+// let K = 2;
+// let n = 5;
+// let S = 0;
+// for (let i = 1; i <= n; i++) {
+//   S += i ** K;
+// }
+
+// console.log(S);
+
+/* For 16.*/
+
+// let N = 7;
+// let S = 0;
+// for (let i = 1; i <= N; i++) {
+//   S += i ** i;
+// }
+// console.log(`Yig'indi ${S}`);
+
+/* For 17.*/
+
+// let a = 2;
+// let b = 8;
+// for (let i = a; i <= b; i++) {
+//   for (let k = a; k <= i; k++) {
+//     console.log(k);
+//   }
+// }
+
+/* While 1.*/
+/* While 2.*/
+
+// let a = 9;
+// let b = 2;
+// let i = a;
+// let counter = 0;
+// while (i >= b) {
+//   i -= b;
+//   counter++;
+// }
+// console.log(`MAX ${counter} ta sig'adi. ${i} Ortib qolgan kesma `);
+
+/* While 4.*/
+
+// let n = 19;
+// let m = 5;
+// let i = n;
+// let counter = 0;
+// while (i >= m) {
+//   i -= m;
+//   counter++;
+// }
+// console.log(`butun qismi ${counter} qoldiq qismi ${i}`);
+
+/* While 5.*/
+
+// let n = 359;
+// let i = n;
+// while (i >= 10) {
+//   i -= 10;
+// }
