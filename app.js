65bc3e831e305bb05f97557f23fd0540f1758c03
qobiljon <qobiljon@gmail.com>
2025-01-28 12:21:19 +0500

    Add commented-out code examples for various programming exercises

--- app.js
+++ app.js
@@ -1,164 +1,505 @@
-/* For 5.*/
+// calc(6);
+// Case1. K butun soni berilgan. Baho natijalarini chiqaruvchi programma tuzing.(1-yomon, 2-qoniqarsiz, 3-qoniqarli, 4-yahshi, 5-a'lo). Agar k soni 1-5 gacha oraliqqa tegishli bo`lmasa "xato" deb chiqarilsin.
 
-// let a = 2;
-// let b = 8;
-// let counter = 0;
+// let number = 3;
+// switch (number) {
+//   case 1:
+//     console.log("yomon");
+//     break;
 
-// for (let i = a; i <= b; i++) {
-//   counter += i;
-// }
-// console.log(`Yig'indi ${counter}`);
+//   case 2:
+//     console.log("qoniqarsiz");
+//     break;
 
-/* For 6.*/
-// let a = 1;
-// let b = 3;
-// let counter = 1;
+//   case 3:
+//     console.log("qoniqarli");
+//     break;
 
-// for (let i = a; i <= b; i++) {
-//   counter *= i;
-// }
-// console.log(`Yig'indi ${counter}`);
+//   case 4:
+//     console.log("yaxshi");
+//     break;
 
-/* For 7.*/
-// let a = 1;
-// let b = 9;
-// let counter = 0;
-
-// for (let i = a; i <= b; i++) {
-//   counter += i ** 2;
-// }
-// console.log(`Yig'indi ${counter}`);
-
-/* For 8.*/
-
-// let n = 6;
-// let content = 1;
-// for (let i = 1 / n; i <= 1 / n; i++) {
-//   content += i;
-// }
-// console.log(`Yig'indi ${content}`);
-
-/* For 9.*/
-// let n = 3;
-// let counter = 1;
-// for (let i = 1; i <= n; i++) {
-//   counter *= 1 + i / 10;
-// }
-// console.log(`Yeg'indi ${counter}`);
-
-/* For 10.*/
-
-// let n = 4;
-// let counter = 0;
-// for (let i = 1; i <= n; i++) {
-//   console.log(`${i}`);
-//   for (let k = 1; k <= i; k++) {
-//     let sum = 2 * k - 1;
-//     counter += sum;
-//     console.log(`${sum}  ${counter}`);
-//   }
-//   console.log(`${i}**2 = ${counter}`);
+//   case 5:
+//     console.log("a'lo");
+//     break;
 // }
 
-/* For 11.*/
-// let n = 5;
-// let a = 2;
-// let sum = 1;
-// for (let i = 1; i <= n; i++) {
-//   sum *= a;
-// }
-// console.log(sum);
+// Case2. Oy raqamini berilgan. Kiritilgan oy qaysi faslga tegishli ekanligini chiqaruvchi programma tuzilsin. (Masalan: 2 chi oy, "qish")
+// let number = 11;
+// switch (number) {
+//   case 1:
+//     console.log("Qish Fasli");
+//     break;
 
-/* For 12.*/
+//   case 2:
+//     console.log("Qish Fasli");
+//     break;
 
-// let n = 4;
-// let a = 2;
-// let sum = 1;
-// for (let i = 1; i <= n; i++) {
-//   sum = i ** a;
-//   console.log(sum);
-// }
+//   case 3:
+//     console.log("Bahor Fasli");
+//     break;
 
-/* For 13.*/
+//   case 4:
+//     console.log("Bahor Fasli");
+//     break;
 
-// let n = 4;
-// let a = 2;
-// let sum = 0;
-// for (let i = 1; i <= n; i++) {
-//   sum += a ** i;
-// }
-// console.log(sum);
+//   case 5:
+//     console.log("Bahor Fasli");
+//     break;
 
-/* For 14.*/
+//   case 6:
+//     console.log("Yoz Fasli");
+//     break;
 
-// let n = 4;
-// let S = 0;
-// let factr = 1;
-// for (let i = 1; i <= n; i++) {
-//   factr *= i;
-//
-// }
-// console.log(`Yig'indi ${factr}`);
+//   case 7:
+//     console.log("Yoz Fasli");
+//     break;
 
-/* For 15.*/
+//   case 8:
+//     console.log("Yoz Fasli");
+//     break;
 
-// let K = 2;
-// let n = 5;
-// let S = 0;
-// for (let i = 1; i <= n; i++) {
-//   S += i ** K;
-// }
+//   case 9:
+//     console.log("Kuz Fasli");
+//     break;
 
-// console.log(S);
+//   case 10:
+//     console.log("Kuz Fasli");
+//     break;
 
-/* For 16.*/
+//   case 10:
+//     console.log("Kuz Fasli");
+//     break;
 
-// let N = 7;
-// let S = 0;
-// for (let i = 1; i <= N; i++) {
-//   S += i ** i;
-// }
-// console.log(`Yig'indi ${S}`);
+//   case 11:
+//     console.log("Qish Fasli");
+//     break;
 
-/* For 17.*/
-
-// let a = 2;
-// let b = 8;
-// for (let i = a; i <= b; i++) {
-//   for (let k = a; k <= i; k++) {
-//     console.log(k);
-//   }
+//   case 12:
+//     console.log("Qish Fasli");
+//     break;
 // }
 
-/* While 1.*/
-/* While 2.*/
+// Case3. Oy raqami berilgan. Shu oyda nechta kun borligini aniqlovchi programma tuzilsin.
 
-// let a = 9;
-// let b = 2;
-// let i = a;
-// let counter = 0;
-// while (i >= b) {
-//   i -= b;
-//   counter++;
+// let number = "mart";
+// switch (number) {
+//   case "mart":
+//     console.log("31");
+//     break;
+
+//   case "aprel":
+//     console.log("30");
+//     break;
+
+//   case "may":
+//     console.log("31");
+//     break;
+
+//   case "iyun":
+//     console.log("30");
+//     break;
+
+//   case "iyul":
+//     console.log("31");
+//     break;
+
+//   case "avgust":
+//     console.log("30");
+//     break;
+
+//   case "sentiyabr":
+//     console.log("31");
+//     break;
+
+//   case "oktyabr":
+//     console.log("30");
+//     break;
+
+//   case "noyabr":
+//     console.log("31");
+//     break;
+
+//   case "dekabr":
+//     console.log("30");
+//     break;
+
+//   case "yanvar":
+//     console.log("31");
+//     break;
+
+//   case "febral":
+//     console.log("28");
+//     break;
 // }
-// console.log(`MAX ${counter} ta sig'adi. ${i} Ortib qolgan kesma `);
 
-/* While 4.*/
+// Case4. Uzinlik birliklari quyidagi tartibda berilgan. 1-desimetr, 2-kilometr, 3-metr, 4-millimeter, 5- santimetr. Uzunlik birligini bildiruvchi son berilgan (1 - 5 oraliqda) va shu birlikdagi kesma uzunligi berilgan (haqiqiy son). Kesmaning uzunligini metrlarda ifodalofchi programma tuzilsin.
 
-// let n = 19;
-// let m = 5;
-// let i = n;
-// let counter = 0;
-// while (i >= m) {
-//   i -= m;
-//   counter++;
+// let number = 4;
+// let L = 300;
+// switch (number) {
+//   case 1:
+//     console.log(`${L} didsimetr = ${L / 10} metr`);
+//     break;
+
+//   case 2:
+//     console.log(`${L} kiometr = ${L * 1000} metr`);
+//     break;
+
+//   case 3:
+//     console.log(` ${L} metr`);
+//     break;
+
+//   case 4:
+//     console.log(`${L} millimetr = ${L / 1000} metr`);
+//     break;
+
+//   case 5:
+//     console.log(`${L} santimetr = ${L / 100} metr`);
+//     break;
 // }
-// console.log(`butun qismi ${counter} qoldiq qismi ${i}`);
 
-/* While 5.*/
+// Case5. Og'irlik birliklari quyidagi tartibda berilgan. 1-kilogramm, 2-milligramm, 3-gramm, 4-tonna, 5- sentner. Og'irlik birligini bildiruvchi soni berilgan va shu birlikdagi og`irlik qiymati berilgan. Og`irlikni kilogramda ifodalofchi programma tuzilsin.
 
-// let n = 359;
-// let i = n;
-// while (i >= 10) {
-//   i -= 10;
+// let number = 3;
+// let M = 300;
+// switch (number) {
+//   case 1:
+//     console.log(` ${M} kiogram`);
+//     break;
+
+//   case 2:
+//     console.log(`${M} milligram = ${M / 1000} kilogram`);
+//     break;
+
+//   case 3:
+//     console.log(`${M} gramm = ${M / 100} kilogram`);
+//     break;
+
+//   case 4:
+//     console.log(`${M} tonna = ${M * 1000} kilogram`);
+//     break;
+
+//   case 5:
+//     console.log(`${M} sentner = ${M * 10} kilogram`);
+//     break;
+// }
+
+// Case6. Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (oy). (Kabisa bo`lmagan yil sanasi kiritiladi). Berilgan sanani ifodalovchi programma tuzilsin. Kabisa yilida 366 kun, kabisa bo'lmagan yilda 365 kun bor bo'ladi.
+
+// let number = 10;
+// let yil = 2025;
+// let kun = 21;
+
+// switch (number) {
+//   case 3:
+//     console.log(`${kun}.0${number}.${yil}`);
+//     break;
+
+//   case 4:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 5:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 6:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 7:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 8:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 9:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 10:
+//     console.log(`${kun}.${number}.${yil}`);
+
+//     break;
+
+//   case 11:
+//     console.log(`${kun}.${number}.${yil}`);
+
+//     break;
+
+//   case 12:
+//     console.log(`${kun}.${number}.${yil}`);
+
+//     break;
+
+//   case 1:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+
+//   case 2:
+//     console.log(`${kun}.0${number}.${yil}`);
+
+//     break;
+// }
+
+// Case7. Ikkita butun son berilgan D (kun) va M (oy). (Kabisa bo`lmagan yil sanasi kiritiladi). Berilgan sanadan keyingi sanani ifodalovchi programma tuzilsin.
+
+// let number = 6;
+// let yil = 2025;
+// let kun = 5;
+
+// switch (number) {
+//   case 3:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+//     break;
+
+//   case 4:
+//     if (kun < 30) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 5:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 6:
+//     if (kun < 30) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 7:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 8:
+//     if (kun < 30) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 9:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 10:
+//     if (kun < 30) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 11:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 12:
+//     if (kun < 30) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 1:
+//     if (kun < 31) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+
+//   case 2:
+//     if (kun < 28) {
+//       console.log(`${kun + 1}.${number} `);
+//     } else {
+//       console.log(`1.0${number + 1} `);
+//     }
+
+//     break;
+// }
+
+// Case8. Robot faqat to`rtta tomonga ko`cha oladi ("s"-shimol, "j"-janub, "q"-sharq, "g"-g'arb) va uchta raqamli kamanda: 0-harakni davom ettir, 1-chapga buril, 2-o`ngga buril. Y - robot yo`nalishi va K - kamanda berilgan. Berilgan kamanda bajarilgandan keying robot holatini aniqlovchi programma tuzilsin.
+
+// let K = "g";
+// let Y = 1;
+// switch (K) {
+//   case "sh":
+//     if (Y == 1) {
+//       console.log(`Shimol, chap tomonga buril`);
+//     } else {
+//       console.log("Shimol o'ng tomonga buril");
+//     }
+//     break;
+
+//   case "j":
+//     if (Y == 1) {
+//       console.log(`Janub, chap tomonga buril`);
+//     } else {
+//       console.log("Janub o'ng tomonga buril");
+//     }
+//     break;
+
+//   case "q":
+//     if (Y == 1) {
+//       console.log(`Sharq, chap tomonga buril`);
+//     } else {
+//       console.log("Sharq o'ng tomonga buril");
+//     }
+//     break;
+
+//   case "g":
+//     if (Y == 1) {
+//       console.log(`G'arb, chap tomonga buril`);
+//     } else {
+//       console.log("G'arb o'ng tomonga buril");
+//     }
+//     break;
+// }
+
+// Case10. O`quv masalalarini aniqlovchi 10-40 gacha butun son berilgan. Son kiritilganda unga mos so`zlarda ifodalovchi programma tuzilsin. ("yigirmata masala", "o`n uchta masala" va h.k.)
+
+// let number = 19;
+
+// switch (number) {
+//   case 10:
+//     console.log("O'nta masala");
+//     break;
+
+//   case 11:
+//     console.log("O'n bitta masala");
+//     break;
+
+//   case 12:
+//     console.log("O'n ikkita masala");
+//     break;
+
+//   case 13:
+//     console.log("O'n uchta masala");
+//     break;
+
+//   case 14:
+//     console.log("O'n tortta masala");
+//     break;
+
+//   case 15:
+//     console.log("O'n beshta masala");
+//     break;
+
+//   case 16:
+//     console.log("O'n oltita masala");
+//     break;
+
+//   case 17:
+//     console.log("O'n yettita masala");
+//     break;
+
+//   case 18:
+//     console.log("O'n sakkista masala");
+//     break;
+
+//   case 19:
+//     console.log("O'n to'qqizta masala");
+//     break;
+
+//   case 20:
+//     console.log("Yigirmatta masala");
+//     break;
+// }
+
+// Case11. 100-999 gacha oraliqdagi sonlarni so'zlarda ifodalovchi programma tuzilsin. (masalan: 123- "bir yuz yigirma uch").
+// let number = 115;
+
+// switch (number) {
+//   case 110:
+//     console.log("Bir Yuzi O'nta masala");
+//     break;
+
+//   case 111:
+//     console.log("Bir Yuzi O'n bitta masala");
+//     break;
+
+//   case 112:
+//     console.log("Bir Yuzi O'n ikkita masala");
+//     break;
+
+//   case 113:
+//     console.log("Bir Yuzi O'n uchta masala");
+//     break;
+
+//   case 114:
+//     console.log("Bir Yuzi O'n tortta masala");
+//     break;
+
+//   case 115:
+//     console.log("Bir Yuzi O'n beshta masala");
+//     break;
+
+//   case 116:
+//     console.log("Bir Yuzi O'n oltita masala");
+//     break;
+
+//   case 117:
+//     console.log("Bir Yuzi O'n yettita masala");
+//     break;
+
+//   case 118:
+//     console.log("Bir Yuzi O'n sakkista masala");
+//     break;
+
+//   case 119:
+//     console.log("Bir Yuzi O'n to'qqizta masala");
+//     break;
+
+//   case 120:
+//     console.log("Bir Yuzi Yigirmata masala");
+//     break;
 // }
